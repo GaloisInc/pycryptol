419dac3192152f815ccaa0f59ccde86ba41817cd
Adam C. Foltzer <acfoltzer@galois.com>
2015-06-24 16:41:58 -0700

    made message rep more consistent, added worker

--- cryptol/cryptol.py
+++ cryptol/cryptol.py
@@ -3,52 +3,62 @@ from zmq import Context, REQ
 
 class Cryptol:
     ctx = Context()
-    req = None
+    main_req = None
+    worker_req = None
 
-    def __init__(self, addr):
-        self.req = self.ctx.socket(REQ)
-        self.req.connect(addr)
+    def __init__(self, addr, main_port):
+        self.main_req = self.ctx.socket(REQ)
+        self.main_req.connect(addr + ':' + str(main_port))
+        self.main_req.send_json({'tag': 'connect'})
+        resp = self.main_req.recv_json()
+        print 'response: ' + str(resp)
+        worker_port = resp['port']
+        self.worker_req = self.ctx.socket(REQ)
+        self.worker_req.connect(addr + ':' + str(worker_port))
 
     def __tag_expr(self, tag, expr):
-        self.req.send_json({'tag': tag, 'expr': expr})
-        resp = self.req.recv_json()
+        self.worker_req.send_json({'tag': tag, 'expr': expr})
+        resp = self.worker_req.recv_json()
         print resp
         return resp
 
     def __from_value(self, val):
         # VBit
-        if isinstance(val, bool):
-            return val
+        if 'bit' in val:
+            return val['bit']
         # VRecord
-        if val['tag'] == 'record':
+        if 'record' in val:
             rec = {}
-            for field in val['fields']:
-                fname = field[0]['contents']
+            for field in val['record']:
+                fname = field[0]['Name']
                 fval = self.__from_value(field[1])
                 rec[fname] = fval
             return rec
         # VTuple
-        if val['tag'] == 'tuple':
+        if 'tuple' in val:
             tup = ()
-            for tval in val['values']:
+            for tval in val['tuple']:
                 tup = tup + (self.__from_value(tval),)
             return tup
         # VSeq
-        if val['tag'] == 'sequence' and val['isWord']:
-            return BitVector(bitlist = val['elements'])
-        if val['tag'] == 'sequence' and not val['isWord']:
-            return [self.__from_value(elt) for elt in val['elements']]
+        if 'sequence' in val and val['sequence']['isWord']:
+            return BitVector(bitlist = [self.__from_value(elt) for elt in val['sequence']['elements']])
+        if 'sequence' in val and not val['sequence']['isWord']:
+            return [self.__from_value(elt) for elt in val['sequence']['elements']]
         # VWord
-        if val['tag'] == 'bitvector':
-            return BitVector(intVal = val['value'], size = val['width'])
+        if 'word' in val:
+            bv = val['word']['bitvector']
+            v = bv['value']
+            w = bv['width']
+            return BitVector(intVal = v % (2**w), size = w)
         return val
 
     def __from_funvalue(self, handle):
         print "trying to build a closure for " + str(handle)
         def apply(arg):
             print "applying " + str(arg) + " to handle " + str(handle) + " in context " + str(self)
-            self.req.send_json({'tag': 'applyFun', 'handle': handle, 'arg': self.__to_value(arg)})
-            val = self.req.recv_json()
+            self.worker_req.send_json({'tag': 'applyFun', 'handle': handle, 'arg': self.__to_value(arg)})
+            val = self.worker_req.recv_json()
             if val['tag'] == 'value':
                 print "value"
                 return self.__from_value(val['value'])
@@ -60,22 +70,20 @@ class Cryptol:
     def __to_value(self, pyval):
         # VBit
         if isinstance(pyval, bool):
-            return pyval
+            return {'bit': pyval}
         # VRecord
         elif isinstance(pyval, dict):
-            val = {'tag': 'record'}
-            val['fields'] = [[{'tag': 'Name', 'contents': k}, self.__to_value(v)] for k,v in pyval.items()]
-            return val
+            return {'record': [[{'Name': k}, self.__to_value(v)] for k,v in pyval.items()]}
         # VTuple
         elif isinstance(pyval, tuple):
-            return {'tag': 'tuple', 'values': [self.__to_value(v) for v in pyval]}
+            return {'tuple': [self.__to_value(v) for v in pyval]}
         # VSeq
         elif isinstance(pyval, list):
             # TODO: assert homogeneous, set isWord
-            return {'tag': 'sequence', 'isWord': False, 'elements': [self.__to_value(v) for v in pyval]}
+            return {'sequence': {'isWord': False, 'elements': [self.__to_value(v) for v in pyval]}}
         # VWord
         elif isinstance(pyval, BitVector):
-            return {'tag': 'bitvector', 'width': pyval.length(), 'value': int(pyval)}
+            return {'word': {'bitvector': {'width': pyval.length(), 'value': int(pyval)}}}
         raise ValueError("Unable to convert Python value into Cryptol value")
 
     def eval(self, expr):
@@ -103,23 +111,31 @@ class Cryptol:
         return self.__tag_expr('sat', expr)
 
     def setopt(self, k, v):
-        self.req.send_json({'tag': 'setOpt', 'key': k, 'value': v})
-        return self.req.recv_json()
+        self.worker_req.send_json({'tag': 'setOpt', 'key': k, 'value': v})
+        return self.worker_req.recv_json()
 
     def load_module(self, filepath):
-        self.req.send_json({'tag': 'loadModule', 'filePath': filepath})
-        return self.req.recv_json()
+        self.worker_req.send_json({'tag': 'loadModule', 'filePath': filepath})
+        self.worker_req.recv_json()
+        self.worker_req.send_json({'tag': 'browse'})
+        tlNames = self.worker_req.recv_json()['decls']['ifDecls'].keys()
+        print tlNames
 
     def browse(self):
-        self.req.send_json({'tag': 'browse'})
-        return self.req.recv_json()
+        self.worker_req.send_json({'tag': 'browse'})
+        return self.worker_req.recv_json()
 
     def exit(self):
-        self.req.send_json({'tag': 'exit'})
-        return self.req.recv_json()
+        self.worker_req.send_json({'tag': 'exit'})
+        return self.worker_req.recv_json()
+
+# Takes a JSON representation of a Schema and returns whether or not
+# the type is a function.
+def is_function(sch):
+    return None
 
 if __name__ == '__main__':
-    cry = Cryptol("tcp://127.0.0.1:5555")
+    cry = Cryptol("tcp://127.0.0.1", 5555)
     print cry
     exp = "\\x -> x + 0x4"
     print "*** eval(" + exp + ")"
